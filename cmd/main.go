62ad9c8f1389574b396ec82615438bd8639361d0
David Xiang <admin@mdavid.cn>
2024-09-10 03:54:11 +0800

    init

---
+++ cmd/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+	. "github.com/HEUDavid/auto-receive-crypto-pay/internal"
+	"github.com/HEUDavid/auto-receive-crypto-pay/model"
+	. "github.com/HEUDavid/go-fsm/pkg/metadata"
+	"github.com/gin-gonic/gin"
+	"io"
+	"log"
+	"net/http"
+)
+
+var PaymentAddress = []string{
+	"0x7853b3736edba9d7ce681f2a90264307694f97f2",
+}
+
+func contains(slice []string, item string) bool {
+	for _, element := range slice {
+		if element == item {
+			return true
+		}
+	}
+	return false
+}
+
+func Webhook(c *gin.Context) {
+	c.Header("Content-Type", "application/json")
+
+	body, err := io.ReadAll(c.Request.Body)
+	fmt.Printf("webhook payload: %s, %v\n", body, err)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"err": err.Error()})
+		return
+	}
+	if len(body) == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"err": "no data"})
+		return
+	}
+
+	task := GenTaskInstance(Adapter.GenID(), "", &ReceiptData{Data: model.Data{RawData: body}})
+	task.Type = c.Query("Persist")
+	task.State = Hook.GetName()
+
+	_response(c, Adapter.Create(c, task), task)
+}
+
+func Query(c *gin.Context) {
+	task := GenTaskInstance(c.Query("request_id"), c.Query("task_id"), &ReceiptData{})
+	_response(c, Adapter.Query(c, task), task)
+}
+
+func _response(c *gin.Context, err error, task interface{}) {
+	if err == nil {
+		c.JSON(http.StatusOK, task)
+	} else {
+		c.JSON(http.StatusOK, map[string]string{"error": err.Error()})
+	}
+}
+
+func init() {
+	InitWorker()
+	InitAdapter()
+}
+
+func main() {
+	Worker.Run()
+	log.Println("[FSM] Worker started...")
+
+	gin.SetMode(gin.DebugMode)
+	r := gin.Default()
+	r.POST("/webhook", Webhook)
+	r.GET("/query", Query)
+	_ = r.Run("127.0.0.1:8080")
+}
