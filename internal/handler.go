3235cc6c2089c76fa8e51fea52c836a2f04de1f6
David Xiang <admin@mdavid.cn>
2024-09-10 03:45:42 +0800

    init

--- internal/handler.go
+++ internal/handler.go
@@ -7,36 +7,53 @@ import (
 )
 
 var (
-	New  = GenState("New", false, newHandler)
-	Send = GenState("Send", false, sendHandler)
-	End  = State[*ReceiptData]{Name: "End", IsFinal: true, Handler: nil}
+	Hook      = GenState("Hook", false, hookHandler)
+	Processed = State[*ReceiptData]{Name: "Processed", IsFinal: true, Handler: nil}
+
+	New      = GenState("New", false, newHandler)
+	GenToken = GenState("GenToken", false, genTokenHandler)
+	End      = State[*ReceiptData]{Name: "End", IsFinal: true, Handler: nil}
 )
 
 var (
-	New2Send = GenTransition(New, Send)
-	Send2End = GenTransition(Send, End)
-	End2End  = GenTransition(End, End)
+	Hook2Processed = GenTransition(Hook, Processed)
+	New2GenToken   = GenTransition(New, GenToken)
+	GenToken2End   = GenTransition(GenToken, End)
 )
 
 var ReceiptFSM = func() FSM[*ReceiptData] {
-	fsm := GenFSM("ReceiptFSM", New)
-	fsm.RegisterState(New, Send, End)
-	fsm.RegisterTransition(New2Send, Send2End, End2End)
+	fsm := GenFSM[*ReceiptData]("Receipt")
+
+	fsm.RegisterState(Hook, Processed)
+	fsm.RegisterTransition(Hook2Processed)
+
+	fsm.RegisterState(New, GenToken, End)
+	fsm.RegisterTransition(New2GenToken, GenToken2End)
 	return fsm
 }()
+
+func hookHandler(task *Task[*ReceiptData]) error {
+	log.Printf("[FSM] State: %s, Task.Data: %s", task.State, _pretty(task.GetData()))
+	task.Data.Comment = "webhook payload"
+	// 检查数据，然后根据合法收据建立新的任务
+	// ...
+	task.State = Processed.GetName()
+	return nil
+}
 
 func newHandler(task *Task[*ReceiptData]) error {
 	log.Printf("[FSM] State: %s, Task.Data: %s", task.State, _pretty(task.GetData()))
 	task.Data.Comment = "receive cryptocurrency"
-	task.State = Send.GetName() // 下一步 发送邮件通知
+	task.State = GenToken.GetName() // 下一步 执行一些动作
 	return nil
 }
 
-func sendHandler(task *Task[*ReceiptData]) error {
+func genTokenHandler(task *Task[*ReceiptData]) error {
 	log.Printf("[FSM] State: %s, Task: %s", task.State, _pretty(task))
 
 	// Invoke RPC interfaces to perform certain operations.
-	log.Println("send mail success")
+	// 生成卡密或者发送商品之类的
+	log.Println("send mail...")
 	task.Data.Comment = "send mail success"
 
 	task.State = End.GetName() // Switch to next state
