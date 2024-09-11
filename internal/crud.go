2d12d6cb70fe22d739554b6e9572799add3a92d0
David Xiang <admin@mdavid.cn>
2024-09-11 12:55:27 +0800

    init

--- internal/crud.go
+++ internal/crud.go
@@ -7,7 +7,7 @@ import (
 
 func GetToken(c Context, db *gorm.DB, fromAddress string) (*ReceiptData, error) {
 	data := &ReceiptData{}
-	if err := db.Table(data.TableName()).Select("Token, ValidFrom, ValidTo").
+	if err := db.Table(data.TableName()).Select("token, valid_from, valid_to").
 		Where("from_address = ?", fromAddress).Find(data).Error; err != nil {
 		return nil, err
 	}
