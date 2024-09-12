0cce0f37b7b23628d69d7e1ed3c21644498c1a22
David Xiang <admin@mdavid.cn>
2024-09-13 07:38:06 +0800

    upt

--- internal/crud.go
+++ internal/crud.go
@@ -8,7 +8,7 @@ import (
 func GetTokenByAddress(c context.Context, db *gorm.DB, fromAddress string) ([]*ReceiptData, error) {
 	var dataList []*ReceiptData
 	if err := db.Table((&ReceiptData{}).TableName()).
-		Select("*").Omit("raw_data").
+		Omit("raw_data").
 		Where("from_address = ?", fromAddress).
 		Find(&dataList).Error; err != nil {
 		return nil, err
@@ -19,9 +19,9 @@ func GetTokenByAddress(c context.Context, db *gorm.DB, fromAddress string) ([]*R
 func GetTokenDetails(c context.Context, db *gorm.DB, token string) (*ReceiptData, error) {
 	data := &ReceiptData{}
 	if err := db.Table((&ReceiptData{}).TableName()).
-		Select("*").Omit("raw_data").
+		Omit("raw_data").
 		Where("token = ?", token).
-		Scan(&data).Error; err != nil {
+		First(data).Error; err != nil {
 		return nil, err
 	}
 	return data, nil
