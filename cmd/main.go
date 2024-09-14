e6c86524e68589a7d0977c5634e79f5c68517572
David Xiang <admin@mdavid.cn>
2024-09-15 07:36:10 +0800

    upt

--- cmd/main.go
+++ cmd/main.go
@@ -3,169 +3,47 @@ package main
 import (
 	"fmt"
 	. "github.com/HEUDavid/auto-receive-crypto-pay/internal"
-	"github.com/HEUDavid/auto-receive-crypto-pay/model"
-	. "github.com/HEUDavid/go-fsm/pkg/metadata"
 	"github.com/HEUDavid/go-fsm/pkg/util"
 	"github.com/gin-gonic/gin"
 	"io"
 	"log"
-	"net/http"
 	"os"
-	"time"
+	"path/filepath"
 )
-
-func Webhook(c *gin.Context) {
-	c.Header("Content-Type", "application/json")
-
-	if c.Query("auth") != GetConfig().Global.Auth {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "auth failed"})
-		return
-	}
-
-	body, err := io.ReadAll(c.Request.Body)
-	log.Printf("webhook payload: %s, %v\n", body, err)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"err": err.Error()})
-		return
-	}
-	if len(body) == 0 {
-		c.JSON(http.StatusBadRequest, gin.H{"err": "no data"})
-		return
-	}
-
-	task := GenTaskInstance(Adapter.GenID(), "", &ReceiptData{Data: model.Data{RawData: body}})
-	task.Type = "Persist"
-	task.State = Hook.GetName()
-
-	response(c, Adapter.Create(c, task), task)
-}
-
-func QueryTask(c *gin.Context) {
-	task := GenTaskInstance(c.Query("request_id"), c.Query("task_id"), &ReceiptData{})
-	response(c, Adapter.Query(c, task), task)
-}
-
-type token struct {
-	Token           string
-	Valid           bool
-	ValidFrom       time.Time
-	ValidTo         time.Time
-	Network         string
-	FromAddress     string
-	ToAddress       string
-	Asset           string
-	Value           float64
-	TransactionTime time.Time
-}
-
-func isValid(validFrom, validTo uint64) bool {
-	currentTime := uint64(time.Now().Unix())
-	return currentTime >= validFrom && currentTime <= validTo
-}
-
-func toToken(data *ReceiptData) *token {
-	if data == nil {
-		return nil
-	}
-	return &token{
-		Token:           data.Token,
-		Valid:           isValid(data.ValidFrom, data.ValidTo),
-		ValidFrom:       time.Unix(int64(data.ValidFrom), 0),
-		ValidTo:         time.Unix(int64(data.ValidTo), 0),
-		Network:         data.Network,
-		FromAddress:     data.FromAddress,
-		ToAddress:       data.ToAddress,
-		Asset:           data.Asset,
-		Value:           data.Value,
-		TransactionTime: time.Unix(int64(data.TransactionTime), 0),
-	}
-}
-
-func checkRequest(c *gin.Context, required ...string) (bool, string) {
-	for _, param := range required {
-		if c.Query(param) == "" {
-			return false, param
-		}
-	}
-	return true, ""
-}
-
-func QueryToken(c *gin.Context) {
-	if valid, missingParam := checkRequest(c, "from_address"); !valid {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": "missing required parameter: " + missingParam,
-		})
-		return
-	}
-
-	dataList, err := GetTokenByAddress(c, Adapter.GetDB(), c.Query("from_address"))
-	var tokens []*token
-	for _, data := range dataList {
-		tokens = append(tokens, toToken(data))
-	}
-	response(c, err, tokens)
-}
-
-func TokenDetails(c *gin.Context) {
-	if valid, missingParam := checkRequest(c, "token"); !valid {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": "missing required parameter: " + missingParam,
-		})
-		return
-	}
-
-	data, err := GetTokenDetails(c, Adapter.GetDB(), c.Query("token"))
-	response(c, err, toToken(data))
-}
-
-func response(c *gin.Context, err error, task interface{}) {
-	if err == nil {
-		c.JSON(http.StatusOK, task)
-	} else {
-		c.JSON(http.StatusOK, map[string]string{"error": err.Error()})
-	}
-}
-
-func Index(c *gin.Context) {
-	c.HTML(http.StatusOK, "index.html", gin.H{
-		"hostRoot":       GetConfig().Global.HostRoot,
-		"adminAddresses": GetConfig().AdminAddress,
-	})
-}
 
 func init() {
 	gin.SetMode(GetConfig().Global.Mode)
 
-	f, _ := os.Create(GetConfig().Global.LogPath)
+	logPath := filepath.Join(util.FindProjectRoot(), GetConfig().Global.LogPath)
+	if err := os.MkdirAll(filepath.Dir(logPath), os.ModePerm); err != nil {
+		panic(fmt.Sprintf("Failed to create log directory: %v", err))
+	}
+
+	f, err := os.OpenFile(logPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to open or create log file: %v", err))
+	}
+
 	mw := io.MultiWriter(os.Stdout, f)
 	gin.DefaultWriter = mw
-	gin.DefaultErrorWriter = mw
-	log.SetOutput(gin.DefaultWriter)
+	log.SetOutput(mw)
 
 	InitWorker()
 	InitAdapter()
 }
 
-func router(path string) string {
-	return fmt.Sprintf("%s/%s", GetConfig().Global.HostRoot, path)
-}
-
-func source(path string) string {
-	return fmt.Sprintf("%s/%s", util.FindProjectRoot(), path)
-}
-
 func main() {
 	r := gin.Default()
 
-	r.POST(router("webhook"), Webhook)
-	r.GET(router("query"), QueryTask)
-	r.GET(router("query_token"), QueryToken)
-	r.GET(router("token_details"), TokenDetails)
+	r.POST(Router("webhook"), Webhook)
+	r.GET(Router("query"), QueryTask)
+	r.GET(Router("query_token"), QueryToken)
+	r.GET(Router("token_details"), TokenDetails)
 
-	r.GET(router("pay"), Index)
+	r.GET(Router("pay"), Index)
 
-	r.Static(router("src"), source("static/src"))
-	r.LoadHTMLGlob(source("static/templates/*"))
+	r.Static(Router("src"), Source("static/src"))
+	r.LoadHTMLGlob(Source("static/templates/*"))
 
 	Worker.Run()
 	log.Println("[FSM] Worker started...")
---
+++ internal/service.go
@@ -0,0 +1,140 @@
+package internal
+
+import (
+	"fmt"
+	"github.com/HEUDavid/auto-receive-crypto-pay/model"
+	. "github.com/HEUDavid/go-fsm/pkg/metadata"
+	"github.com/HEUDavid/go-fsm/pkg/util"
+	"github.com/gin-gonic/gin"
+	"io"
+	"log"
+	"net/http"
+	"time"
+)
+
+func Webhook(c *gin.Context) {
+	c.Header("Content-Type", "application/json")
+
+	if c.Query("auth") != GetConfig().Global.Auth {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "auth failed"})
+		return
+	}
+
+	body, err := io.ReadAll(c.Request.Body)
+	log.Printf("webhook payload: %s, %v\n", body, err)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"err": err.Error()})
+		return
+	}
+	if len(body) == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"err": "no data"})
+		return
+	}
+
+	task := GenTaskInstance(Adapter.GenID(), "", &ReceiptData{Data: model.Data{RawData: body}})
+	task.Type = "Persist"
+	task.State = Hook.GetName()
+
+	response(c, Adapter.Create(c, task), task)
+}
+
+func QueryTask(c *gin.Context) {
+	task := GenTaskInstance(c.Query("request_id"), c.Query("task_id"), &ReceiptData{})
+	response(c, Adapter.Query(c, task), task)
+}
+
+type token struct {
+	Token           string
+	Valid           bool
+	ValidFrom       time.Time
+	ValidTo         time.Time
+	Network         string
+	FromAddress     string
+	ToAddress       string
+	Asset           string
+	Value           float64
+	TransactionTime time.Time
+}
+
+func isValid(validFrom, validTo uint64) bool {
+	currentTime := uint64(time.Now().Unix())
+	return currentTime >= validFrom && currentTime <= validTo
+}
+
+func toToken(data *ReceiptData) *token {
+	if data == nil {
+		return nil
+	}
+	return &token{
+		Token:           data.Token,
+		Valid:           isValid(data.ValidFrom, data.ValidTo),
+		ValidFrom:       time.Unix(int64(data.ValidFrom), 0),
+		ValidTo:         time.Unix(int64(data.ValidTo), 0),
+		Network:         data.Network,
+		FromAddress:     data.FromAddress,
+		ToAddress:       data.ToAddress,
+		Asset:           data.Asset,
+		Value:           data.Value,
+		TransactionTime: time.Unix(int64(data.TransactionTime), 0),
+	}
+}
+
+func checkRequest(c *gin.Context, required ...string) (bool, string) {
+	for _, param := range required {
+		if c.Query(param) == "" {
+			return false, param
+		}
+	}
+	return true, ""
+}
+
+func QueryToken(c *gin.Context) {
+	if valid, missingParam := checkRequest(c, "from_address"); !valid {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "missing required parameter: " + missingParam,
+		})
+		return
+	}
+
+	dataList, err := GetTokenByAddress(c, Adapter.GetDB(), c.Query("from_address"))
+	var tokens []*token
+	for _, data := range dataList {
+		tokens = append(tokens, toToken(data))
+	}
+	response(c, err, tokens)
+}
+
+func TokenDetails(c *gin.Context) {
+	if valid, missingParam := checkRequest(c, "token"); !valid {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "missing required parameter: " + missingParam,
+		})
+		return
+	}
+
+	data, err := GetTokenDetails(c, Adapter.GetDB(), c.Query("token"))
+	response(c, err, toToken(data))
+}
+
+func response(c *gin.Context, err error, task interface{}) {
+	if err == nil {
+		c.JSON(http.StatusOK, task)
+	} else {
+		c.JSON(http.StatusOK, map[string]string{"error": err.Error()})
+	}
+}
+
+func Index(c *gin.Context) {
+	c.HTML(http.StatusOK, "index.html", gin.H{
+		"hostRoot":       GetConfig().Global.HostRoot,
+		"adminAddresses": GetConfig().AdminAddress,
+	})
+}
+
+func Router(path string) string {
+	return fmt.Sprintf("%s/%s", GetConfig().Global.HostRoot, path)
+}
+
+func Source(path string) string {
+	return fmt.Sprintf("%s/%s", util.FindProjectRoot(), path)
+}
