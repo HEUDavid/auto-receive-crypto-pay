5df2ce7ff206338b433bbf04728fdfa2d5063a2d
David Xiang <admin@mdavid.cn>
2024-09-15 08:36:48 +0800

    upt

--- test/test.sh
+++ test/test.sh
@@ -3,10 +3,10 @@ curl -X POST http://localhost:8080/webhook?auth=auth_key \
      -H "Content-Type: application/json" \
      --data @data/Test_ETH_MAINNET.json
 
-# 根据转账地址查询token
+# 根据转账地址查询 invoice
 bash -c 'curl -s "$1" | python -m json.tool' \
      -- "http://localhost:8080/query_token?from_address=0x71660c4005ba85c37ccec55d0c4493e66fe775d3"
 
-# 查询 token 详情
+# 查询 invoice 详情
 bash -c 'curl -s "$1" | python -m json.tool' \
      -- "http://localhost:8080/token_details?token=942ef637afad1f6ac3860c4dd8a0ff74"
