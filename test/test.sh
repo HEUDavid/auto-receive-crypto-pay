2fb5e785181b9284d127532eacab7c5c40d6cd74
David Xiang <admin@mdavid.cn>
2024-09-15 08:38:31 +0800

    upt

--- test/test.sh
+++ test/test.sh
@@ -5,8 +5,8 @@ curl -X POST http://localhost:8080/webhook?auth=auth_key \
 
 # 根据转账地址查询 invoice
 bash -c 'curl -s "$1" | python -m json.tool' \
-     -- "http://localhost:8080/query_token?from_address=0x71660c4005ba85c37ccec55d0c4493e66fe775d3"
+     -- "http://localhost:8080/query_invoice?from_address=0x71660c4005ba85c37ccec55d0c4493e66fe775d3"
 
 # 查询 invoice 详情
 bash -c 'curl -s "$1" | python -m json.tool' \
-     -- "http://localhost:8080/token_details?token=942ef637afad1f6ac3860c4dd8a0ff74"
+     -- "http://localhost:8080/invoice_details?invoice=942ef637afad1f6ac3860c4dd8a0ff74"
