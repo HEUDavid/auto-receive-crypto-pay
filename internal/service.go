7f7e6e99df7e7e324d18d065f441056e58337537
David Xiang <admin@mdavid.cn>
2024-11-25 03:54:20 +0800

    Feature: RMQ supports automatic reconnection

--- internal/service.go
+++ internal/service.go
@@ -1,142 +1,90 @@
 package internal
 
 import (
-	"fmt"
+	"context"
 	"github.com/HEUDavid/auto-receive-crypto-pay/model"
+	"github.com/HEUDavid/go-fsm/pkg"
+	db "github.com/HEUDavid/go-fsm/pkg/db/mysql"
 	. "github.com/HEUDavid/go-fsm/pkg/metadata"
+	mq "github.com/HEUDavid/go-fsm/pkg/mq/rmq"
 	"github.com/HEUDavid/go-fsm/pkg/util"
-	"github.com/gin-gonic/gin"
-	"io"
 	"log"
-	"net/http"
+	"sync"
 	"time"
 )
 
-func Webhook(c *gin.Context) {
-	c.Header("Content-Type", "application/json")
+type IService interface{ DoInit() }
+type Service struct{ __init__ sync.Once }
 
-	if c.Query("auth") != GetConfig().Global.Auth {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "auth failed"})
-		return
-	}
+func (s *Service) DoInit() { panic("not implemented") }
 
-	body, err := io.ReadAll(c.Request.Body)
-	log.Printf("webhook payload: %s, %v\n", body, err)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"err": err.Error()})
-		return
-	}
-	if len(body) == 0 {
-		c.JSON(http.StatusBadRequest, gin.H{"err": "no data"})
-		return
-	}
-
-	task := GenTaskInstance(Adapter.GenID(), "", &ReceiptData{Data: model.Data{RawData: body}})
-	task.Type = "Persist"
-	task.State = Hook.GetName()
-
-	response(c, Adapter.Create(c, task), task)
+type ServiceWorker struct {
+	Service
+	pkg.Worker[*ReceiptData]
 }
 
-func QueryTask(c *gin.Context) {
-	task := GenTaskInstance(c.Query("request_id"), c.Query("task_id"), &ReceiptData{})
-	response(c, Adapter.Query(c, task), task)
-}
-
-type invoice struct {
-	Network         string
-	FromAddress     string
-	ToAddress       string
-	Asset           string
-	Value           float64
-	InvoiceID       string
-	ValidFrom       time.Time
-	ValidTo         time.Time
-	TransactionTime time.Time
-	Content         string
-	Valid           bool
-}
-
-func isValid(validFrom, validTo uint64) bool {
-	currentTime := uint64(time.Now().Unix())
-	return currentTime >= validFrom && currentTime <= validTo
-}
-
-func toInvoice(data *ReceiptData) *invoice {
-	if data == nil {
-		return nil
-	}
-	return &invoice{
-		Network:         data.Network,
-		FromAddress:     data.FromAddress,
-		ToAddress:       data.ToAddress,
-		Asset:           data.Asset,
-		Value:           data.Value,
-		InvoiceID:       data.InvoiceID,
-		ValidFrom:       time.Unix(int64(data.ValidFrom), 0),
-		ValidTo:         time.Unix(int64(data.ValidTo), 0),
-		TransactionTime: time.Unix(int64(data.TransactionTime), 0),
-		Content:         data.InvoiceID,
-		Valid:           isValid(data.ValidFrom, data.ValidTo),
-	}
-}
-
-func checkRequest(c *gin.Context, required ...string) (bool, string) {
-	for _, param := range required {
-		if c.Query(param) == "" {
-			return false, param
-		}
-	}
-	return true, ""
-}
-
-func QueryInvoice(c *gin.Context) {
-	if valid, missingParam := checkRequest(c, "from_address"); !valid {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": "missing required parameter: " + missingParam,
-		})
-		return
-	}
-
-	dataList, err := GetInvoiceByAddress(c, Adapter.GetDB(), c.Query("from_address"))
-	var invoices []*invoice
-	for _, data := range dataList {
-		invoices = append(invoices, toInvoice(data))
-	}
-	response(c, err, invoices)
-}
-
-func InvoiceDetails(c *gin.Context) {
-	if valid, missingParam := checkRequest(c, "invoice"); !valid {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": "missing required parameter: " + missingParam,
-		})
-		return
-	}
-
-	data, err := GetInvoiceDetails(c, Adapter.GetDB(), c.Query("invoice"))
-	response(c, err, toInvoice(data))
-}
-
-func response(c *gin.Context, err error, task interface{}) {
-	if err == nil {
-		c.JSON(http.StatusOK, task)
-	} else {
-		c.JSON(http.StatusOK, map[string]string{"error": err.Error()})
-	}
-}
-
-func Index(c *gin.Context) {
-	c.HTML(http.StatusOK, "index.html", gin.H{
-		"hostRoot":       GetConfig().Global.HostRoot,
-		"adminAddresses": GetConfig().AdminAddress,
+func (s *ServiceWorker) DoInit() {
+	s.__init__.Do(func() {
+		s.RegisterModel(
+			&ReceiptData{},
+			&model.Task{},
+			&model.UniqueRequest{},
+		)
+		s.RegisterFSM(ReceiptFSM)
+		s.RegisterGenerator(util.UniqueID)
+		s.RegisterDB(&db.Factory{Section: "mysql"})
+		s.RegisterMQ(&mq.Factory{Section: "rmq"})
+		s.Config = util.GetConfig()
+		s.Init()
 	})
 }
 
-func Router(path string) string {
-	return fmt.Sprintf("%s/%s", GetConfig().Global.HostRoot, path)
+func NewWorker() *ServiceWorker {
+	w := &ServiceWorker{}
+	w.MaxGoroutines = 20
+	return w
 }
 
-func Source(path string) string {
-	return fmt.Sprintf("%s/%s", util.FindProjectRoot(), path)
+type ServiceAdapter struct {
+	Service
+	pkg.Adapter[*ReceiptData]
+}
+
+func (s *ServiceAdapter) DoInit() {
+	s.__init__.Do(func() {
+		s.RegisterModel(
+			&ReceiptData{},
+			&model.Task{},
+			&model.UniqueRequest{},
+		)
+		s.RegisterFSM(ReceiptFSM)
+		s.RegisterGenerator(util.UniqueID)
+		s.RegisterDB(&db.Factory{Section: "mysql"})
+		s.RegisterMQ(&mq.Factory{Section: "rmq"})
+		s.Config = util.GetConfig()
+		_ = s.Init()
+	})
+}
+
+func (a *ServiceAdapter) BeforeCreate(c context.Context, task *Task[*ReceiptData]) error {
+	log.Println("[FSM] Rewrite BeforeCreate...")
+	task.Version = 1
+	task.Data.TransactionTime = uint64(time.Now().Unix())
+	return nil
+}
+
+func NewAdapter() *ServiceAdapter {
+	a := &ServiceAdapter{}
+	a.ReBeforeCreate = a.BeforeCreate
+	return a
+}
+
+var (
+	Worker  = NewWorker()
+	Adapter = NewAdapter()
+)
+
+func init() {
+	Worker.DoInit()
+	Adapter.DoInit()
 }
